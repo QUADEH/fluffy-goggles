4b49ecd1e7aef227494d4cb56dd3dfeb4ef76c7f
QUADEH <127901701+QUADEH@users.noreply.github.com>
2023-04-05 18:59:40 -0500

    Update 14Things.js

--- 14Things.js
+++ 14Things.js
@@ -1,10 +1,19 @@
 const dogs = [{ name: 'Snickers', age: 2 }, { name: 'hugo', age: 8 }];
+let makeGrn = document.querySelector("#makeGreen");
 
-    function makeGreen() {
-      const p = document.querySelector('p');
-      p.style.color = '#BADA55';
-      p.style.fontSize = '50px';
-    }
+function makeGreen() {
+  const p = document.querySelector('p');
+  p.style.color = '#BADA55';
+  p.style.fontSize = '50px';
+}
+
+function handleMakeGreen() {
+  makeGreen();
+  let output = "Text is now green!";
+  console.log(output);
+}
+
+makeGrn.addEventListener('click', handleMakeGreen);
 
     // Regular
     console.log('hello');
